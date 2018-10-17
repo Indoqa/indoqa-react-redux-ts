cc2b95d84d9cda3834348a7faf4cadcf82558de2
Reinhard Pötz <reinhard.poetz@indoqa.com>
2018-10-17 19:46:56 +0200

    cleanup code

--- src/main/forms/store/forms.epics.ts
+++ src/main/forms/store/forms.epics.ts
@@ -9,22 +9,19 @@ import {FormsAction, FormsActionKeys} from './forms.types'
 
 interface FormsEpic extends Epic<Types.RootAction, FormsAction, Types.RootState, Types.Services> {}
 
+const doSaveForm = (action: SaveUser, history: any): Observable<FormsAction> => {
+  if (action.user.name.startsWith('G')) {
+    action.setErrors({name: 'Names starting with \'G\' are not allowed.'})
+    return of(saveUserError())
+  }
+  history.push('/forms/users/')
+  return of(saveUserSuccess(action.user))
+}
+
 const fetchWordsEpic$: FormsEpic = (action$, state, {history}) =>
   action$.pipe(
     ofType<SaveUser>(FormsActionKeys.SAVE_USER),
-    mergeMap((action) => {
-      // early returns within the if statement do not work with Typescript, hence assigning
-      // the observable to a variable
-      let nextAction: Observable<FormsAction>
-      if (action.user.name.startsWith('G')) {
-        action.setErrors({name: 'Names starting with \'G\' are not allowed.'})
-        nextAction = of(saveUserError())
-      } else {
-        history.push('/forms/users/')
-        nextAction = of(saveUserSuccess(action.user))
-      }
-      return nextAction
-    }),
+    mergeMap((action) => doSaveForm(action, history)),
   )
 
 export default [fetchWordsEpic$]
